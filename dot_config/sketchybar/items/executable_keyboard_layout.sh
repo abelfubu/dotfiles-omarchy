de36aaa5d04c4059b68a7e0d62165f2b27d0254f
Abel de la Fuente <abel.delafuente@easor.com>
2026-01-03 09:34:07 +0100

    feat: 2026-01-03T09:34

--- dot_config/sketchybar/items/executable_keyboard_layout.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sketchybar --add event keyboard_change 'AppleSelectedInputSourcesChangedNotification'
-
-sketchybar --add item keyboard_layout right \
-  --set keyboard_layout script="$PLUGIN_DIR/keyboard_layout.sh" \
-  --subscribe keyboard_layout keyboard_change
